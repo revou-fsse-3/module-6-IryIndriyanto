e5d74dac8382ebaf91b7a74946458bcd743b8b67
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-02-14 01:34:26 -0700

    migration to poetry

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
 
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/")
-def check():
-    return "Flask is working"
+    @app.route("/")
+    def hello_world():
+        return "Hello !!!"
 
-
-if __name__ == "__main__":
-    app.run()
+    return app
