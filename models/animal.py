4ca20310cd1a735367f53928ed99bd3d96fe8706
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-02-15 04:56:50 -0700

    FEAT : create animal model database

---
+++ models/animal.py
@@ -0,0 +1,9 @@
+from db import db
+
+class AnimalModel(db.Model):
+    __tablename__ = "animals"
+
+    id = db.Column(db.integer, primary_key=True)
+    species = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
