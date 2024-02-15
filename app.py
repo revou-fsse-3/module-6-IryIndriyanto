3c030e7823449475b67e68f386d4f9d306a52106
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-02-15 04:56:09 -0700

    CONFIG : initialize flask app

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from dotenv import load_dotenv
+from flask_smorest import Api
+from db import db
+from resources.animal import blp as animal_blueprint
+import os
 
 
 def create_app():
     app = Flask(__name__)
+    load_dotenv()
 
-    @app.route("/")
-    def hello_world():
-        return "Hello !!!"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    api = Api(app)
+    api.register_blueprint(animal_blueprint)
 
     return app
