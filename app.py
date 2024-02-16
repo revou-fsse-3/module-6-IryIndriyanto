59535a5e6932a3706b4aa891cd7320cabe3e82fa
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-02-16 06:49:41 -0700

    FEAT : make endpoint for animals/id

--- app.py
+++ app.py
@@ -17,9 +17,14 @@ def create_app():
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/docs"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL","sqlite:///data.db")
+    app.config["OPENAPI_SWAGGER_UI_URL"] = (
+        "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    )
+
+    # DATABASE_URL = os.getenv("DATABASE_URL").replace("")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
 
     db.init_app(app)
     with app.app_context():
--- resources/animal.py
+++ resources/animal.py
@@ -2,7 +2,7 @@ from flask_smorest import Blueprint, abort
 from flask.views import MethodView
 from models import AnimalModel
 from db import db
-from sqlalchemy.exc import IntegrityError,SQLAlchemyError
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from schemas import AnimalSchema
 
 blp = Blueprint(
@@ -11,10 +11,11 @@ blp = Blueprint(
 
 
 @blp.route("/")
-class Animals(MethodView):
-    @blp.response(200, AnimalSchema (many=True))
+class Animal(MethodView):
+    @blp.response(200, AnimalSchema(many=True))
     def get(self):
         return AnimalModel.query.all()
+
     @blp.arguments(AnimalSchema)
     @blp.response(200, AnimalSchema)
     def post(self, animal_data):
@@ -29,3 +30,36 @@ class Animals(MethodView):
             abort(500, "an error occurred while creating")
 
         return animal
+
+@blp.route("/<int:animal_id>")
+class Animal(MethodView):
+    @blp.response(200, AnimalSchema)
+    def get(self, animal_id):
+        try:
+            animal = AnimalModel.query.get(animal_id)
+            return animal
+        except KeyError:
+            abort(404, "animal not found")
+
+    @blp.arguments(AnimalSchema)
+    @blp.response(200, AnimalSchema)
+    def put(self, animal_data, animal_id):
+        try:
+            animal = AnimalModel.query.get(animal_id)
+            AnimalModel.query.filter_by(id=animal_id).update(animal_data)
+            db.session.commit()
+            return animal
+        except KeyError:
+            abort(404, "animal not found")
+
+    @blp.response(200, AnimalSchema)
+    def delete(self, animal_id):
+        try:
+            animal = AnimalModel.query.get(animal_id)
+            db.session.delete(animal)
+            db.session.commit()
+            return {"massage": "animal deleted"}
+        except KeyError:
+            abort(404, "animal not found")
+
+
